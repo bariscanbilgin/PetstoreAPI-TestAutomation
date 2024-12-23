8c9193c1c1330fb6fe2d61bd44c5c7176e7035bb
Baris Can Bilgin <bcbilgin@yandex.com>
2024-12-23 18:07:04 +0300

    delete user
    delete user function created

--- src/test/java/PetstoreApiTest.java
+++ src/test/java/PetstoreApiTest.java
@@ -5,23 +5,29 @@ import static io.restassured.RestAssured.given;
 
 public class PetstoreApiTest {
 
-    private String url = "https://petstore.swagger.io/v2";
-    private String username = "QA-Testing-BCB";
-    private int userId = 232;
+    private String url          = "https://petstore.swagger.io/v2";
+    private int    userId       = 232;
+    private String username     = "QA-Testing-BCB";
+    private String firstName    = "Baris Can";
+    private String lastNAme     = "Bilgin";
+    private String userMail     = "QA-Testing-BCB@yandex.com";
+    private String userPassword = "test123";
+    private String userMobile   = "+90 500 500 50 50";
+
 
     @Test
     public void createUser() {
         System.out.println("creating user.");
         String userData =
-                "{\n" +
-                "\"id\" : 232," +
-                "\"username\": \"" + username + "\",\n" +
-                "\"firstName\": \"Baris Can\"," +
-                "\"lastName\": \"Bilgin\"," +
-                "\"email\": \"QA-Testing-BCB@yandex.com\"," +
-                "\"password\": \"test123\"," +
-                "\"phone\": \"+90 500 500 50 50\"," +
-                "\"userStatus\": 1 " +
+                "{\n"                   +
+                "\"id\"        :   "    + userId        + ",\n"   +
+                "\"username\"  : \""    + username      + "\",\n" +
+                "\"firstName\" : \""    + firstName     + "\",\n" +
+                "\"lastName\"  : \""    + lastNAme      + "\",\n" +
+                "\"email\"     : \""    + userMail      + "\",\n" +
+                "\"password\"  : \""    + userPassword  + "\",\n" +
+                "\"phone\"     : \""    + userMobile    + "\",\n" +
+                "\"userStatus\": 1 "    +
                 "}";
 
         given().body(userData).contentType(ContentType.JSON).when()
@@ -31,6 +37,7 @@ public class PetstoreApiTest {
     @Test
     public  void readUser(){
         System.out.println("reading user data.");
+
     }
 
     @Test
@@ -41,5 +48,7 @@ public class PetstoreApiTest {
     @Test
     public void deleteUser(){
         System.out.println("deleting user");
+
+        given().when().delete(url + "/user/" + username).then().statusCode(200);
     }
 }
